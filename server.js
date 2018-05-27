664ac457972c858d4a820f8b0c2794e0c692a045
tylergreulich <tgreul6@gmail.com>
2018-05-27 16:41:41 -0700

    heroku

--- server.js
+++ server.js
@@ -80,8 +80,6 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(`${port}`, () => {
-  console.log(`Listening on Port: ${port}`);
-});
+app.listen(process.env.PORT || 5000);
 
 module.exports = app;
